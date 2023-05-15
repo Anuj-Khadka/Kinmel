69f120e83efd97b5e1779b2512d50e2cafcfcad6
Anuj-Khadka <anujkhadka008@gmail.com>
2023-05-15 23:47:02 +0545

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-import { useState } from 'react'
-import './App.css'
+import { Component, useState } from "react";
+import "./App.css";
+import { BrowserRouter, Router, Route } from "react-router-dom";
+import Header from "./components/Header";
+import ProductListing from "./components/ProductListing";
+import ProductComponent from "./components/ProductComponent";
+import ProductDetails from "./components/ProductDetail";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-    
-    </>
-  )
+    <BrowserRouter>
+      <Header />
+      <Router>
+        <Route path="/" component={ProductListing} />
+        <Route path="/product/productId" component={ProductComponent} />
+        <Route>404 Not Found</Route>
+      </Router>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
