62f76a5ffcfa509c021fb2ce61d2c6511bec7cb8
Anuj-Khadka <anujkhadka008@gmail.com>
2023-05-16 22:31:42 +0545

    update app

--- src/redux/actions/product-actions.js
+++ src/redux/actions/product-actions.js
@@ -7,9 +7,14 @@ export const setProducts = (products) => {
   };
 };
 
-export const selectedProducts = (product) => {
+export const selectedProduct = (product) => {
   return {
     type: actionTypes.SELECTED_PRODUCT,
     payload: product,
+  };
+};
+export const removeSelectedProduct = (product) => {
+  return {
+    type: actionTypes.REMOVE_SELECTED_PRODUCT,
   };
 };
