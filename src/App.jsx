a00dcdc36f8bdeb2be4b6d415bfa78f084d772f7
Anuj Khadka <anujkhadka008@gmail.com>
2023-05-17 22:43:03 +0545

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
   return (
     <Router>
       <Header />
+    
       <Routes>
         <Route path="/" element={<ProductListing />} />
         <Route path="/product/:productId" element={<ProductDetails />} />
