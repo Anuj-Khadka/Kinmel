3ceb922cd275e2d8e41153b751e35d19067b67f9
Anuj-Khadka <anujkhadka008@gmail.com>
2023-05-21 23:42:09 +0545

    add middleware

--- src/redux/actions/product-actions.js
+++ src/redux/actions/product-actions.js
@@ -1,4 +1,27 @@
 import { actionTypes } from "../consts/action-types";
+import fakestoreapi from "../../apis/fake-store-api";
+
+export const fetchProducts = () => {
+  return async (dispatch)=>{
+    const response = await fakestoreapi.get("/products")
+    dispatch({type: actionTypes.FETCH_PRODUCTS, payload: response.data})
+  }
+  // return {
+  //   type: actionTypes.FETCH_PRODUCTS,
+  //   payload: response,
+  // };
+};
+
+export const fetchProduct = (id) => {
+  return async (dispatch)=>{
+    const response = await fakestoreapi.get(`/products/${id}`)
+    dispatch({type: actionTypes.SELECTED_PRODUCT, payload: response.data})
+  }
+  // return {
+  //   type: actionTypes.FETCH_PRODUCTS,
+  //   payload: response,
+  // };
+};
 
 export const setProducts = (products) => {
   return {
