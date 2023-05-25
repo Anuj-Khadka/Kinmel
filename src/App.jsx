a11abb20c319f58ac7a0bff68884dc2352dd0fae
Anuj-Khadka <anujkhadka008@gmail.com>
2023-05-25 22:12:38 +0545

    update ui

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,23 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import ProductListing from "./components/ProductListing";
 import ProductDetails from "./components/ProductDetails";
+import HomeGallery from "./components/HomeGallery";
 
 function App() {
   return (
-    <Router>
-      <Header />    
-      <Routes>
+    <div className="main-container">
+      <Router>
+        <Header />
+        <div>
+          <HomeGallery />
+          {/* <Routes>
         <Route path="/" element={<ProductListing />} />
         <Route path="/product/:productId" element={<ProductDetails />} />
         <Route>404 Not Found</Route>
-      </Routes>
-    </Router>
+      </Routes> */}
+        </div>
+      </Router>
+    </div>
   );
 }
 
