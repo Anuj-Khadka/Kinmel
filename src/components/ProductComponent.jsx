bd1ff0e5b145329315ea28fcaf97708c4af564a5
Anuj-Khadka <anujkhadka008@gmail.com>
2023-05-26 22:59:05 +0545

    update ui

--- src/components/ProductComponent.jsx
+++ src/components/ProductComponent.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import {MdStar, MdStarOutline, MdStarRate} from "react-icons/md";
 
 const ProductComponent = () => {
   const products = useSelector((state) => state.allProducts.products);
   const renderList = products.map((product) => {
-    const { id, title, image, price, category } = product;
+    const { id, title, image, price, category, rating } = product;
+
+    // let rateContainer = document.getElementById("rating")
+    // for (let i = 0; i < Math.ceil(rating.rate); i++) {
+    //   rateContainer.appendChild(<MdStarRate/>)
+    // }
+
     return (
       <div className="four wide column product-item" key={id}>
         <Link to={`/product/${id}`}>
@@ -14,10 +21,21 @@ const ProductComponent = () => {
               <div className="product-image">
                 <img src={image} alt={title} />
               </div>
-              <div className="content">
-                <div className="header">{title}</div>
-                <div className="meta price">$ {price}</div>
+              <div className="product-content">
+                <div className="header">
+                  <h4 className="product-name">{title}</h4>
+                  <span className="product-price">$ {price}</span>
+                </div>
                 <div className="meta">{category}</div>
+                <div className="rating" id="rating">
+                  {/* {rateContainer} */}
+                  <MdStar />
+                  <MdStarOutline />
+                  <span className="rating-count">({rating.count})</span>
+                </div>
+              </div>
+              <div className="add-to-cart">
+                <button className="add-cart-btn">Add to Cart</button>
               </div>
             </div>
           </div>
@@ -25,11 +43,7 @@ const ProductComponent = () => {
       </div>
     );
   });
-  return(
-    <>
-    {renderList}
-    </>
-  )
+  return <>{renderList}</>;
 };
 
 export default ProductComponent;
