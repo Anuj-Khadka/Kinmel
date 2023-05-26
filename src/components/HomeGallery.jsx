8abe74befc4568a8553a3876d3fe54b52d3f5f2b
Anuj-Khadka <anujkhadka008@gmail.com>
2023-05-26 21:39:20 +0545

    update ui

--- src/components/HomeGallery.jsx
+++ src/components/HomeGallery.jsx
@@ -4,11 +4,17 @@ import "../assets/css/HomeGallery.css"
 const HomeGallery = () => {
   return (
     <div className="gallery-container">
-      <div className="main-gallery gallery-carousel"></div>
-      <div className="category-gallery">
-        <div className="top-category"></div>
-        <div className="bottom-category"></div>
+      <div className="main-gallery gallery-carousel">
+        <img src="https://gibranmallick.com/wp-content/uploads/2020/06/Sunnto-Watch-Product-Photography_Gibran-Mallick-Studio_Photo-Video-Web-Media-Design-e1602962059761.jpg" alt="watch" /> 
       </div>
+      {/* <div className="category-gallery">  */}
+        <div className="category-gallery" id="top-category">
+          <img src="https://images.pexels.com/photos/2885014/pexels-photo-2885014.jpeg?cs=srgb&dl=pexels-garrett-morrow-2885014.jpg&fm=jpg" alt="pc_gaming_collection" /> 
+        </div>
+        <div className="category-gallery" id="bottom-category">
+          <img src="https://ecommercephotographyindia.com/blog/wp-content/uploads/2021/11/black-black-wallpaper-camera-8792-1024x683-1.jpeg" alt="accessories" /> 
+        </div>
+      {/* </div> */}
     </div>
   );
 };
