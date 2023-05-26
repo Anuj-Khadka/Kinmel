b3168064f0a0bd5a2c2e523fec0bff60b8aef218
Anuj-Khadka <anujkhadka008@gmail.com>
2023-05-26 21:39:26 +0545

    update ui

--- src/components/main-components/Home.jsx
+++ src/components/main-components/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HomeGallery from '../HomeGallery'
+import ProductListing from '../ProductListing'
 
 const Home = () => {
   return (
     <div>
     <HomeGallery />
+    <ProductListing />
     </div>
   )
 }
