9c388b923b133d3f6e34de4ed07c3dfa35581706
Anuj-Khadka <anujkhadka008@gmail.com>
2023-05-27 17:37:45 +0545

    update ui

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,9 +1,10 @@
 import { combineReducers } from "redux";
-import { productReducer, selectedProductReducer } from "./product-reducer";
+import { productReducer, selectedProductReducer, fetchCategories } from "./product-reducer";
 
 const reducers = combineReducers({
   allProducts: productReducer,
   product: selectedProductReducer,
+  categories: fetchCategories,
 });
 
 export default reducers;
