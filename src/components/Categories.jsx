51349c174f89e6869a7986460d088a5eea1b46d6
Anuj-Khadka <anujkhadka008@gmail.com>
2023-05-27 21:57:33 +0545

    update ui

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,27 +1,51 @@
-import React, { useEffect } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchCategories } from "../redux/actions/product-actions";
 import "../assets/css/Products.css";
+import { categoryListContext } from "./ProductListing";
 
 const Categories = () => {
   const categories = useSelector((state) => state.categories.categories);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchCategories());
+
+    return () => {
+      return null;
+    };
   }, []);
+
+  const { filterProducts } = useContext(categoryListContext);
+
+  const clickAllHandler = () => {
+    filterProducts("all");
+  };
+
   console.log(categories);
 
-  const categoriesList = categories.map((category) => {
-    return (
-      <li className="category" key={category}>
-        {category.toUpperCase()}
-      </li>
-    );
-  });
   return (
     <div className="categories-container">
       <h2 className="categories-heading">Category</h2>
-      <ul className="categories">{categoriesList}</ul>
+      <ul className="categories">
+        <li className="category" key="all" onClick={() => clickAllHandler()}>
+          ALL
+        </li>
+        {categories &&
+          categories.map((category) => {
+            const clickHandler = () => {
+              filterProducts(category);
+            };
+            return (
+              <li
+                className="category"
+                key={category}
+                onClick={() => clickHandler()}
+              >
+                {category.toUpperCase()}
+              </li>
+            );
+          })}
+      </ul>
     </div>
   );
 };
