5605f4ea7ac9fc15025f8a40348e98c8a8c094a0
Anuj-Khadka <anujkhadka008@gmail.com>
2023-06-08 02:37:01 +0545

    update product ui

--- src/components/ProductComponent.jsx
+++ src/components/ProductComponent.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { MdStar, MdStarOutline, MdStarRate } from "react-icons/md";
@@ -14,6 +14,18 @@ const ProductComponent = ({ cat }) => {
 
   const renderList = products.map((product) => {
     const { id, title, image, price, category, rating } = product;
+
+    const filledStars = [];
+    const outlineStars = [];
+    // const rates = Math.ceil(rating.rate)
+    const ratingRate = rating?.rate || 0; // Perform null check on rating.rate
+    for (let i = 0; i < ratingRate; i++) {
+      filledStars.push(<MdStar key={i} color="#f46a5b" />);
+    }
+    for (let i = 0; i < 4 - ratingRate; i++) {
+      outlineStars.push(<MdStarOutline key={i} color="#f46a5b" />);
+    }
+
     if (category === cat || cat === "all" || !cat) {
       return (
         <motion.div
@@ -38,8 +50,10 @@ const ProductComponent = ({ cat }) => {
                   <div className="meta">{category}</div>
                   <div className="rating" id="rating">
                     {/* {rateContainer} */}
-                    <MdStar />
-                    <MdStarOutline />
+                    {/* <MdStar />
+                    <MdStarOutline /> */}
+                    {filledStars}
+                    {outlineStars}
                     <span className="rating-count">({rating.count})</span>
                   </div>
                 </div>
